69f1e25dcf90c0035b668964a83257e3940c76f4
Tanjil <tanjilkh@gmail.com>
2024-11-01 15:05:13 +0000

    scripts: include other models

--- seeders.js
+++ seeders.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const User = require("./models/User");
+const Appointment = require("./models/Appointment");
+const Repair = require("./models/Repair");
 
 dotenv.config();
 
@@ -13,11 +15,15 @@ mongoose.connect(process.env.MONGO_URI).then((conn) => {
 
 // Read JSON files
 const users = JSON.parse(fs.readFileSync("./data/users.json"));
+const appointments = JSON.parse(fs.readFileSync("./data/appointments.json"));
+const repairs = JSON.parse(fs.readFileSync("./data/repairs.json"));
 
 // Import Data
 const importData = async () => {
   try {
     await User.create(users);
+    await Appointment.create(appointments);
+    await Repair.create(repairs);
     console.log("Data imported successfully");
     process.exit();
   } catch (err) {
@@ -28,6 +34,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await User.deleteMany();
+    await Appointment.deleteMany();
+    await Repair.deleteMany();
     console.log("Data successfuly deleted!");
     process.exit();
   } catch (err) {
