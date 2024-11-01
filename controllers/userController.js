fd526f254812c79f242e7c9714e74aaec61ef071
Tanjil <tanjilkh@gmail.com>
2024-11-01 16:25:01 +0000

    feat: create and delete users routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -48,6 +48,28 @@ const getUser = async function (req, res) {
   }
 };
 
+const createUser = async function (req, res) {
+  try {
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: err,
+    });
+  }
+};
+
 const updateUser = async function (req, res) {
   try {
     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
@@ -74,8 +96,34 @@ const updateUser = async function (req, res) {
   }
 };
 
+const deleteUser = async function (req, res) {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+      });
+    } else {
+      res.status(204).json({
+        status: "success",
+        message: "User deleted successfully",
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      status: "error",
+      message: err,
+    });
+    return;
+  }
+};
+
 module.exports = {
   getAllUsers,
   getUser,
+  createUser,
   updateUser,
+  deleteUser,
 };
